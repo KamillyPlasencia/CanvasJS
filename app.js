4ca75a7e82c3199faf1944768036c7e997e1e567
KamillyPlasencia <136420965+KamillyPlasencia@users.noreply.github.com>
2023-06-13 21:45:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,107 @@
-let isDay = true; // Variable para controlar si es de día o de noche
-let sunPosition = 0; // Posición del sol (0 representa el amanecer y el atardecer)
+let sunPosX;
+let moonPosX;
+let isDay = true;
+let stars = [];
 
 function setup() {
   createCanvas(800, 600);
+  sunPosX = width / 4;
+  moonPosX = width - width / 4;
+
+  // Crear estrellas
+  for (let i = 0; i < 100; i++) {
+    stars.push(createVector(random(width), random(height)));
+  }
 }
 
 function draw() {
+  background(0);
+  
   if (isDay) {
-    // Fondo de día
-    background(135, 206, 235); // Cielo azul claro
-
-    // Sol
-    fill(255, 255, 0); // Amarillo
-    noStroke();
-    let sunX = map(sunPosition, 0, width, -100, width + 100);
-    let sunY = map(sunPosition, 0, width, height + 50, -50);
-    ellipse(sunX, sunY, 100, 100);
-
-    // Movimiento de los árboles (ejemplo)
-    drawTree(width / 2, height, 200);
+    drawDay();
   } else {
-    // Fondo de noche
-    background(0); // Cielo negro
-
-    // Luna
-    fill(255); // Blanco
-    noStroke();
-    let moonX = map(sunPosition, 0, width, -100, width + 100);
-    let moonY = map(sunPosition, 0, width, height + 50, -50);
-    ellipse(moonX, moonY, 80, 80);
-
-    // Movimiento de las estrellas (ejemplo)
-    drawStars();
+    drawNight();
   }
+  
+  // Árboles
+  drawTree(100, height - 50, 0.5);
+  drawTree(200, height - 100, 0.4);
+  drawTree(600, height - 70, 0.6);
+  drawTree(700, height - 120, 0.3);
+  
+  // Montaña
+  drawMountain(width / 2, height, 1);
+  
+  //drawCloud(200,200);
+}
 
-  // Actualizar la posición del sol
-  sunPosition += 1;
-  if (sunPosition > width) {
-    sunPosition = 0;
-    isDay = !isDay; // Cambiar entre día y noche
+function drawDay() {
+  // Sol
+  fill(255, 255, 24);
+  ellipse(sunPosX, height / 1, 200);
+  
+  // Transición del cielo
+  let skyColor = lerpColor(color(135, 206, 235), color(0), map(sunPosX, width / 4, width - width / 4, 0, 1));
+  background(skyColor);
+  
+  // Actualizar posición del sol
+  sunPosX += 1;
+  if (sunPosX > width - width / 3) {
+    isDay = false;
   }
 }
 
-// Función para dibujar un árbol en una posición dada
-function drawTree(x, y, height) {
-  fill(139, 69, 19); // Marrón oscuro
-  rect(x - 10, y, 20, -height); // Tronco
-
-  fill(34, 139, 34); // Verde oscuro
-  let radius = 80;
-  let angle = PI / 4;
-  ellipse(x, y - height, radius, radius); // Copa del árbol
-  ellipse(x + radius * cos(angle), y - height - radius * sin(angle), radius, radius);
-  ellipse(x - radius * cos(angle), y - height - radius * sin(angle), radius, radius);
+function drawNight() {
+  // Luna
+  fill(200);
+  ellipse(moonPosX, height / 3, 130);
+  
+  // Estrellas
+  fill(255);
+  for (let i = 0; i < stars.length; i++) {
+    let star = stars[i];
+    ellipse(star.x, star.y, 2);
+  }
+  
+  // Actualizar posición de la luna
+  moonPosX -= 1;
+  if (moonPosX < width / 4) {
+    isDay = true;
+  }
 }
 
-// Función para dibujar estrellas en posiciones aleatorias
-function drawStars() {
-  fill(255); // Blanco
+function drawTree(x, y) {
+  // Dibuja un árbol en las coordenadas (x, y)
+  push();
+  translate(x, y);
+  fill(109, 69, 1);
+  rect(-10, 0, 20, 100);
+  fill(331, 139, 34);
+  triangle(-50, 0, 0, -100, 50, 0);
+  pop();
+}
+
+function drawMountain(x, y) {
+  // Dibuja una montaña en las coordenadas (x, y)
+  push();
+  translate(x, y);
+  fill(0, 170, 0);
+  triangle(-210, 3, 3, -230, 200, 6);
+  
+}
+function drawCloud (x,y) {
+  
   noStroke();
-  for (let i = 0; i < 100; i++) {
-    let x = random(width);
-    let y = random(height);
-    ellipse(x, y, 3, 3);
-  }
+  
+  fill(255,255,255);
+  
+  // Círculos externos
+  ellipse(x - 20, y, 50, 50);
+  ellipse(x, y - 20, 50, 50);
+  ellipse(x + 20, y, 50, 50);
+  
+  // Círculos internos
+  ellipse(x, y, 50, 50);
+  ellipse(x + 40, y, 50, 50);
 }
+
